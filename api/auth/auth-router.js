fe57587fbc1f6894de3a15613e58f371dbccb479
John Merz <jmerz826@gmail.com>
2022-02-01 18:40:14 -0500

    minor change

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -10,7 +10,7 @@ router.post('/login', (req, res, next) => {
         .then(([user]) => {
             if (user && bcrypt.compareSync(password, user.password)) {
                 const token = tokenBuilder(user)
-                res.status(200).json({message: `welcome, ${username}`, token})
+                res.status(200).json({message: `welcome back, ${username}`, token})
             } else {
                 next({status: 401, message: 'invalid credentials'})
             }
