d5b8aec60b8a1d753b2c43084a9a18c58e1014e5
John Merz <jmerz826@gmail.com>
2022-01-31 21:29:00 -0500

    cleaned

--- api/server.js
+++ api/server.js
@@ -4,14 +4,7 @@ const cors = require('cors')
 const db = require('./data/db-config')
 const usersRouter = require('./users/users-router')
 
-function getAllUsers() { return db('users') }
 
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
 
 const server = express()
 server.use(express.json())
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -36,6 +36,15 @@ router.post("/register", (req, res, next) => {
   } catch (err) {
     next(err);
   }
+    // ---------------- FROM TEMPLATE ---------------- 
+    // function getAllUsers() { return db('users') }
+
+// async function insertUser(user) {
+//   // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
+//   // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
+//   const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
+//   return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
+// }
 });
 
 module.exports = router;
