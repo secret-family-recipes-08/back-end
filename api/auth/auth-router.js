debec3c698f914178189f28700897312a33851f0
John Merz <jmerz826@gmail.com>
2022-02-01 18:38:48 -0500

    fixed bcrypt bug

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -8,7 +8,7 @@ router.post('/login', (req, res, next) => {
     
     User.getBy({ username })
         .then(([user]) => {
-            if (user && bcrypt.compareSync(password)) {
+            if (user && bcrypt.compareSync(password, user.password)) {
                 const token = tokenBuilder(user)
                 res.status(200).json({message: `welcome, ${username}`, token})
             } else {
