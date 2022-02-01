af29a637a0cd9d4effece086f2681ab96155c48a
John Merz <jmerz826@gmail.com>
2022-02-01 18:27:29 -0500

    built getBy(filter) user-model function

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
+const User = require('../users/users-model')
 
-
+router.post('/login', (req, res, next) => {
+    let { username, password } = req.body
+    
+    
+})
 
 module.exports = router
