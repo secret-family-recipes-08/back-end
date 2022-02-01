0d92129047d92c7d5365bf0af1373569778030b9
John Merz <jmerz826@gmail.com>
2022-01-31 21:27:05 -0500

    can fetch and register new users

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -4,4 +4,13 @@ function getAll() {
     return db('users')
 }
 
-module.exports = {getAll}
+function getById(id) {
+    return db('users').where('user_id', id).first()
+}
+
+async function add(user) {
+    const [newUser] = await db('users').insert(user, ['user_id', 'username', 'password'])
+    return newUser
+}
+
+module.exports = {getAll, getById, add}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,18 +1,41 @@
-const router = require('express').Router()
-const User = require('./users-model')
+const router = require("express").Router();
+const User = require("./users-model");
 
-router.get('/', async (req, res, next) => {
-    try {
-        const users = await User.getAll()
-        if (!users) {
-            next({message:'could not get users'})
-        } else {
-            res.status(200).json(users)
-        }
-    } catch (err) {
-        next(err)
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await User.getAll();
+    if (!users) {
+      next({ message: "could not get users" });
+    } else {
+      res.status(200).json(users);
     }
-    
-})
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+router.get("/:id", (req, res, next) => {
+  User.getById(req.params.id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/register", (req, res, next) => {
+  try {
+    User.add(req.body)
+      .then((newUser) => {
+        res
+          .status(201)
+          .json({
+            message: `New user: ${newUser.username} registered successfully!`,
+          });
+      })
+      .catch((err) => next(err));
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
