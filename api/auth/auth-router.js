394f576f166a9bc2cae0f7dde8f74c4f9ece8458
John Merz <jmerz826@gmail.com>
2022-02-01 18:34:15 -0500

    login endpoint built

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../users/users-model')
 const bcrypt = require('bcrypt')
+const tokenBuilder = require('./token-builder')
 
 router.post('/login', (req, res, next) => {
     let { username, password } = req.body
@@ -8,7 +9,10 @@ router.post('/login', (req, res, next) => {
     User.getBy({ username })
         .then(([user]) => {
             if (user && bcrypt.compareSync(password)) {
-                
+                const token = tokenBuilder(user)
+                res.status(200).json({message: `welcome, ${username}`, token})
+            } else {
+                next({status: 401, message: 'invalid credentials'})
             }
         })
         .catch(err => next(err))
