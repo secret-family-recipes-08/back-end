1fb8bdd059282a1b57e489fdac26c86f0f00fd7c
John Merz <jmerz826@gmail.com>
2022-01-31 23:29:16 -0500

    get recipe by ID endpoint working

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,10 +1,19 @@
-const db = require('../data/db-config')
+const db = require("../data/db-config");
 
 function findAll() {
-    return db('recipes as r')
-        .leftJoin('categories as c', 'r.category_id', 'c.category_id')
-        .leftJoin('sources as s', 'r.source_id', 's.source_id')
-        .select('r.recipe_name', 'c.category_name', 's.source_name')
+  return db("recipes as r")
+    .leftJoin("categories as c", "r.category_id", "c.category_id")
+    .leftJoin("sources as s", "r.source_id", "s.source_id")
+    .select("r.recipe_name", "c.category_name", "s.source_name");
 }
 
-module.exports = {findAll}
+function findById(id) {
+  return db("recipes as r")
+    .leftJoin("categories as c", "r.category_id", "c.category_id")
+    .leftJoin("sources as s", "r.source_id", "s.source_id")
+    .select("r.recipe_name", "c.category_name", "s.source_name")
+    .where("r.recipe_id", id)
+    .first();
+}
+
+module.exports = { findAll, findById };
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -9,4 +9,16 @@ router.get('/', (req, res, next) => {
         .catch(err => next(err))
 })
 
+router.get('/:id', (req, res, next) => {
+    const {id} = req.params
+    Recipe.findById(id)
+        .then(recipe => {
+            if (!recipe) {
+                return next({ status: 404, message: `could not find recipe with id ${id}`})
+            }
+            res.status(200).json(recipe)
+        })
+        .catch(err => next(err))
+})
+
 module.exports = router
