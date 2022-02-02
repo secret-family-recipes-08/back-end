2318e4aeaeb4e8c8db7d290f4d0ea2cc37bb0921
John Merz <jmerz826@gmail.com>
2022-02-02 16:26:19 -0500

    middleware verifying recipe title not already used

--- api/middleware/recipes-middleware.js
+++ api/middleware/recipes-middleware.js
@@ -1,19 +1,40 @@
-function validateRecipe(req, res, next) {
-    const { recipe_name:title, source_id:source, recipe_ingredients: ingredients, recipe_instructions: instructions,  category_id:category } = req.body
-    if (
-        !title || !title.trim() || 
-        !source || !source.trim() || 
-        !ingredients || !ingredients.trim() || 
-        !instructions || !instructions.trim() || 
-        !category || !category.trim()) {
-            res.status(400).json({
-                message: 'title, ingredients, instructions, source, and category are required'
-            })
+const Recipe = require("../recipes/recipes-model");
+
+async function validateRecipe(req, res, next) {
+  const {
+    recipe_name: title,
+    source_id: source,
+    recipe_ingredients: ingredients,
+    recipe_instructions: instructions,
+    category_id: category,
+  } = req.body;
+
+  if (
+    !title ||
+    !title.trim() ||
+    !source ||
+    !source.trim() ||
+    !ingredients ||
+    !ingredients.trim() ||
+    !instructions ||
+    !instructions.trim() ||
+    !category ||
+    !category.trim()
+  ) {
+    res.status(400).json({
+      message:
+        "title, ingredients, instructions, source, and category are required",
+    });
+  } else {
+      const [exists] = await Recipe.findBy({"recipe_name": title});
+    if (!exists) {
+      next();
     } else {
-        next()
+        next({status: 400, message: 'recipe with this title already exists'})
     }
+  }
 }
 
 module.exports = {
-    validateRecipe
-}
+  validateRecipe,
+};
