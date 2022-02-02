f615df19e6dcbeded478fb57f34feca913bb1ade
John Merz <jmerz826@gmail.com>
2022-02-02 15:05:16 -0500

    modified migration

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -13,11 +13,6 @@ exports.up = async (knex) => {
       table.increments("source_id");
       table.string("source_name", 256).notNullable().unique();
     })
-    .createTable("ingredients", (table) => {
-      table.increments("ingredient_id");
-      table.string("ingredient_name", 256).notNullable().unique();
-      table.string("ingredient_unit", 256);
-    })
     .createTable("recipes", (table) => {
       table.increments("recipe_id");
       table.string("recipe_name", 256).notNullable().unique();
@@ -28,58 +23,22 @@ exports.up = async (knex) => {
         .notNullable()
         .references("source_id")
         .inTable("sources")
-        .onDelete("RESTRICT")
-        .onUpdate("RESTRICT");
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
       table
         .integer("category_id")
         .unsigned()
         .notNullable()
         .references("category_id")
         .inTable("categories")
-        .onDelete("RESTRICT")
-        .onUpdate("RESTRICT");
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
     })
-    .createTable("instructions", (table) => {
-      table.increments("instruction_id");
-      table.integer("instruction_number").notNullable();
-      table.string("instruction_text", 1000).notNullable();
-      table
-        .integer("recipe_id")
-        .unsigned()
-        .notNullable()
-        .references("recipe_id")
-        .inTable("recipes")
-        .onDelete("RESTRICT")
-        .onUpdate("RESTRICT");
-    })
-    .createTable("quantities", (table) => {
-      table.increments("quantity_id");
-      table
-        .integer("instruction_id")
-        .unsigned()
-        .notNullable()
-        .references("instruction_id")
-        .inTable("instructions")
-        .onDelete("RESTRICT")
-        .onUpdate("RESTRICT");
-      table
-        .integer("ingredient_id")
-        .unsigned()
-        .notNullable()
-        .references("ingredient_id")
-        .inTable("ingredients")
-        .onDelete("RESTRICT")
-        .onUpdate("RESTRICT");
-      table.float("quantity").notNullable();
-    });
 };
 
 exports.down = async (knex) => {
   await knex.schema
-    .dropTableIfExists("quantities")
-    .dropTableIfExists("instructions")
     .dropTableIfExists("recipes")
-    .dropTableIfExists("ingredients")
     .dropTableIfExists("sources")
     .dropTableIfExists("categories")
     .dropTableIfExists("users");
