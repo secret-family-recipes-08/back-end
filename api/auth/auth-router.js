8373a5f38199d94b952dbc1c9c7a0699cac31733
John Merz <jmerz826@gmail.com>
2022-02-01 22:39:57 -0500

    cleaned code

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,14 +1,9 @@
 const router = require('express').Router()
-const User = require('../users/users-model')
 const bcrypt = require('bcrypt')
 const tokenBuilder = require('./token-builder')
 const {checkUsernameExists, validateUser} = require('../middleware/users-middleware')
 
 router.post('/login', validateUser, checkUsernameExists, (req, res, next) => {
-    // let { username, password } = req.body
-    
-    // User.getBy({ username })
-    // .then(([user]) => {
     try {
         const {username, password} = req.user
         if (bcrypt.compareSync(req.body.password, password)) {
