3eea5074b07b2e61e1168455df9bfb02d310548b
John Merz <jmerz826@gmail.com>
2022-02-02 13:43:40 -0500

    findById recipe returning more data

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -22,7 +22,18 @@ function findById(id) {
   return db("recipes as r")
     .leftJoin("categories as c", "r.category_id", "c.category_id")
     .leftJoin("sources as s", "r.source_id", "s.source_id")
-    .select("r.recipe_name", "c.category_name", "s.source_name")
+    .leftJoin("instructions as instr", "r.recipe_id", "instr.recipe_id")    
+    .select(
+      "r.recipe_id",
+      "r.recipe_name",
+      "c.category_id",
+      "c.category_name",
+      "s.source_id",
+      "s.source_name",
+      "instr.instruction_id",
+      "instr.instruction_number",
+      "instr.instruction_text"
+    )
     .where("r.recipe_id", id)
     .first();
 }
