57631d8f2a825bae7edb2a674d4b8431f620a0ca
Nelson Maldonado <play2nino@gmail.com>
2022-02-02 17:35:54 -0800

    most of put working, all delete by id working

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,4 +1,4 @@
-const db = require("../data/db-config");
+const db = require("../data/db-config")
 
 function findAll() {
   return db("recipes as r")
@@ -13,7 +13,7 @@ function findAll() {
       "c.category_name",
       "s.source_id",
       "s.source_name"
-    );
+    )
 }
 
 function findById(id) {
@@ -30,17 +30,37 @@ function findById(id) {
       "s.source_id",
       "s.source_name"
     )
-    .where("r.recipe_id", id);
+    .where("r.recipe_id", id)
 }
 
 async function add(newRecipe) {
-  const [added] = await db("recipes").insert(newRecipe, ["recipe_name"]);
-  return added;
+  const [added] = await db("recipes").insert(newRecipe, ["recipe_name"])
+  return added
 }
 
+async function update(id, recipe) {
+  console.log("Before: ", await findById(id))
+  await db("recipes").where("recipe_id", id).update({
+    recipe_name: recipe.recipe_name,
+    recipe_instructions: recipe.recipe_instructions,
+    recipe_ingredients: recipe.recipe_ingredients,
+    // category_id: recipe.category_id,
+    // category_name: recipe.category_name,
+    // source_id: recipe.source_id,
+    // source_name: recipe.source_name,
+  })
+  console.log("After: ", await findById(id))
+  return findById(id)
+}
 
+// async function edit(id, changes) {
+//   await db('items').where("item_id", id).update(changes)
+//   return findById(id)
+// }
+//const update = async (recipe) => db("recipes").where({ id: recipe.id }).update(recipe).then(() => recipe);
 
-const deleteById = id => {
-return db("recipes").where('recipe_id', id).del()}
+const deleteById = (id) => {
+  return db("recipes").where("recipe_id", id).del()
+}
 
-module.exports = { findAll, findById, add, deleteById };
+module.exports = { findAll, findById, add, deleteById, update }
